d67ba1b715d143018aacb9f6a12470ce9e39e55d
MathiasDWaterlaw <mathiasdellosa@gmail.com>
2024-08-05 01:35:51 +0200

    Trying to bettering the textarea auto resizing height

--- src/components/NoteEditor.jsx
+++ src/components/NoteEditor.jsx
@@ -1,5 +1,5 @@
 import "./NoteEditor.css";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 import { useHeading, useParagraph } from "../context/NoteContext";
 
 function NoteEditor() {
@@ -15,28 +15,64 @@ function NoteEditor() {
     }
   }, [headingState, paragraphState]);
 
+  const headingRef = useRef();
+  const paragraphRef = useRef();
+
+  const handleResize = () => {
+    headingRef.current.style.height = "auto";
+    paragraphRef.current.style.height = "auto";
+
+    headingRef.current.style.height = headingRef.current.scrollHeight + "px";
+    paragraphRef.current.style.height =
+      paragraphRef.current.scrollHeight + "px";
+  };
+
+  // useLayoutEffect(() => {
+  //   const growers = document.querySelectorAll(".grow-wrap");
+
+  //   growers.forEach((grower) => {
+  //     const textarea = grower.querySelector("textarea");
+  //     textarea.addEventListener("input", () => {
+  //       grower.dataset.replicatedValue = textarea.value;
+  //     });
+  //   });
+  // }, [headingState, paragraphState, window.innerWidth]);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize); //necessary to prevent errors when the page change.
+  }, [headingState, paragraphState, window.innerWidth]);
+
   return (
     <div className='NoteEditor'>
-      <textarea
-        // ref={headingRef}
-        className='input-area note-heading'
-        // contentEditable
-        onChange={(e) => {
-          setHeadingState(e.target.value);
-        }}
-        value={headingState}
-        placeholder='Title...'
-        rows={1}
-      ></textarea>
+      <div className='grow-wrap'>
+        <textarea
+          ref={headingRef}
+          className='input-area note-heading'
+          onChange={(e) => {
+            setHeadingState(e.target.value);
+          }}
+          value={headingState}
+          placeholder='Title...'
+          rows={1}
+          // onResize={handleResize}
+        ></textarea>
+      </div>
 
-      <textarea
-        className='input-area note-paragraph'
-        onChange={(e) => {
-          setParagraphState(e.target.value);
-        }}
-        placeholder='Add your text here...'
-        value={paragraphState}
-      ></textarea>
+      <div className='grow-wrap'>
+        <textarea
+          ref={paragraphRef}
+          className='input-area note-paragraph'
+          onChange={(e) => {
+            setParagraphState(e.target.value);
+          }}
+          placeholder='Add your text here...'
+          value={paragraphState}
+          rows={4}
+          // onResize={handleResize}
+        ></textarea>
+      </div>
     </div>
   );
 }
--- src/context/NoteContext.jsx
+++ src/context/NoteContext.jsx
@@ -5,8 +5,12 @@ const ParagraphContext = createContext();
 const StorageContext = createContext();
 
 export default function NoteContextProvider({ children }) {
-  const [headingState, setHeadingState] = useState("Asdrubale Barca");
-  const [paragraphState, setParagraphState] = useState("");
+  const [headingState, setHeadingState] = useState(
+    "Capitolo 1 - Sotto i cieli di Barad"
+  );
+  const [paragraphState, setParagraphState] = useState(
+    "Lorem ipsum odor amet, consectetuer adipiscing elit. Odio interdum non convallis porttitor cubilia fames. Nisi leo torquent quis, odio aliquet laoreet. Platea et suspendisse sollicitudin parturient maximus adipiscing quis netus velit. Amet sodales tellus ante lectus ridiculus! Lectus nibh egestas sed mattis suscipit per. Rutrum viverra nec ultrices natoque adipiscing semper laoreet. Natoque elit iaculis netus malesuada vehicula ultrices lacus maecenas. Felis vel porttitor curae senectus consequat fermentum praesent parturient suspendisse."
+  );
 
   return (
     <HeadingContext.Provider value={[headingState, setHeadingState]}>
